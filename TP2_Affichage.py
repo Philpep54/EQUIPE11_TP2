f5a2f0601a131277c5c35f971b8a36a3ccf93ff9
Phillipe Pépin <phillipe.pepin.1@ulaval.ca>
2021-04-13 14:05:26 -0400

    .

--- TP2_Affichage.py
+++ TP2_Affichage.py
@@ -80,30 +80,38 @@ def ar_Var_dy():
 
 #------------------------Affichage------------------------#
 
-""" DataFrame """
-# print(f"\n{res[12]}\n")
-# print(f"\nX:\n {df_Var_X}")
-# print(f"\nY:\n {df_Var_Y}")
-# print(f"\nAij:\n {df_Var_Aij}")
-# print(f"\nBij:\n {df_Var_Bij}")
-# print(f"\ndx:\n {df_Var_dx}")
-# print(f"\ndy:\n {df_Var_dy}")
+def pr_df():
+    print(f"\n{res[12]}\n")
+    print(f"\nX:\n {df_Var_X}")
+    print(f"\nY:\n {df_Var_Y}")
+    print(f"\nAij:\n {df_Var_Aij}")
+    print(f"\nBij:\n {df_Var_Bij}")
+    print(f"\ndx:\n {df_Var_dx}")
+    print(f"\ndy:\n {df_Var_dy}")
 
-""" Dictionnaire """
-# print(f"\n{res[12]}\n")
-# print(f"\nX:\n {dic_Var_X}")
-# print(f"\nY:\n {dic_Var_Y}")
-# print(f"\nAij:\n {dic_Var_Aij}")
-# print(f"\nBij:\n {dic_Var_Bij}")
-# print(f"\ndx:\n {dic_Var_dx}")
-# print(f"\ndy:\n {dic_Var_dy}")
+def pr_dic():
+    print(f"\n{res[12]}\n")
+    print(f"\nX:\n {dic_Var_X}")
+    print(f"\nY:\n {dic_Var_Y}")
+    print(f"\nAij:\n {dic_Var_Aij}")
+    print(f"\nBij:\n {dic_Var_Bij}")
+    print(f"\ndx:\n {dic_Var_dx}")
+    print(f"\ndy:\n {dic_Var_dy}")
 
-""" Array """
-print(f"\n{res[12]}\n")
-print(f"\n{ar_Var_X()} X")
-print(f"\n{ar_Var_Y()} Y")
-print(f"\n{ar_Var_Aij()} Aij")
-print(f"\n{ar_Var_Bij()} Bij")
-print(f"\n{ar_Var_dx()} dx")
-print(f"\n{ar_Var_dy()} dy")
+def pr_ar():
+    print(f"\n{res[12]}\n")
+    print(f"\n{ar_Var_X()} X")
+    print(f"\n{ar_Var_Y()} Y")
+    print(f"\n{ar_Var_Aij()} Aij")
+    print(f"\n{ar_Var_Bij()} Bij")
+    print(f"\n{ar_Var_dx()} dx")
+    print(f"\n{ar_Var_dy()} dy")
 
+""" Afficher les DataFrame """
+# pr_df()
+
+""" Afficher les Dictionnaire """
+# pr_dic()
+
+""" Afficher les Array """
+pr_ar()
