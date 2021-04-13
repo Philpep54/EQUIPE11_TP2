9358ff6cac7c7590b88298d8073a18d6341fc581
Phillipe Pépin <phillipe.pepin.1@ulaval.ca>
2021-04-13 14:44:16 -0400

    drette dans database

--- src/TP2_BD.py
+++ src/TP2_BD.py
@@ -2,7 +2,7 @@ import sqlite3
 import os
 import numpy as np
 
-db_path = os.path.normpath('./qc_configs.sqlite')
+db_path = os.path.normpath('/Users/phillipepepin/Documents/UL_2020-2021/Hiver 2021/Environnement de développement de SIAD/SIAD_TP2/TP2_db.db')
 c = sqlite3.connect(db_path)
 
 #Create Table
--- src/TP2_pytosql.py
+++ src/TP2_pytosql.py
@@ -1,11 +1,17 @@
 import sqlite3
 import TP2_dat
 
+n = 6
+m = 3
+d = 12
+L = 27
+lon_u = 24
+lar_u = 60
 set_K = [1,2,3,4,5,6]
 larg_on = [4,3,5,4,6,5]
 poids = [0,4,5,2,3,6,4,0,4,3,5,3,5,4,0,4,7,8,2,3,4,0,3,3,3,5,7,3,0,5,6,3,8,3,5,0]
 
-simulation1 = TP2_dat.Solver(set_K, larg_on, poids)
+simulation1 = TP2_dat.Solver(n, m, d, L, lon_u, lar_u, set_K, larg_on, poids)
 
 list_data = simulation1.solve()
 
@@ -30,7 +36,7 @@ Dy = str(y)
 bd_data = [X, Y, Aij, Bij, Dx, Dy]
 
 
-connexion = sqlite3.connect('./qc_configs.sqlite')
+connexion = sqlite3.connect('/Users/phillipepepin/Documents/UL_2020-2021/Hiver 2021/Environnement de développement de SIAD/SIAD_TP2/TP2_db.db')
 
 cursor = connexion.cursor()
 
