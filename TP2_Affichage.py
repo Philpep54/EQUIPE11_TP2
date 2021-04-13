bef352e0ca40d56e1d9313d35872a86ae950a350
Phillipe Pépin <phillipe.pepin.1@ulaval.ca>
2021-04-13 12:58:46 -0400

    .

--- TP2_Affichage.py
+++ TP2_Affichage.py
@@ -1,4 +1,5 @@
 from TP2_dat import Solver
+import numpy as np
 
 #-------------------------Données-------------------------#
 
@@ -11,19 +12,77 @@ poids = [0,4,5,2,3,6,4,0,4,3,5,3,5,4,0,4,7,8,2,3,4,0,3,3,3,5,7,3,0,5,6,3,8,3,5,0
 inst = Solver(set_K, larg_on, poids)
 res = inst.solve()
 
-Var_X = res[0]
-Var_Y = res[1]
-Var_Aij = res[2]
-Var_Bij = res[3]
-Var_dx = res[4]
-Var_dy = res[5]
+""" DataFrame """
+df_Var_X = res[6]
+df_Var_Y = res[7]
+df_Var_Aij = res[8]
+df_Var_Bij = res[9]
+df_Var_dx = res[10]
+df_Var_dy = res[11]
+
+""" Dictionnaire """
+dic_Var_X = res[0]
+dic_Var_Y = res[1]
+dic_Var_Aij = res[2]
+dic_Var_Bij = res[3]
+dic_Var_dx = res[4]
+dic_Var_dy = res[5]
+
+""" Array """
+upd_Var_X = []
+for i in dic_Var_X.values():
+    upd_Var_X.append(i)
+ar_Var_X = np.reshape(upd_Var_X, len(set_K))
+
+upd_Var_Y = []
+for i in dic_Var_Y.values():
+    upd_Var_Y.append(i)
+ar_Var_Y = np.reshape(upd_Var_Y, len(set_K))
+
+upd_Var_Aij = []
+for i in dic_Var_Aij.values():
+    upd_Var_Aij.append(i)
+ar_Var_Aij = np.reshape(upd_Var_Aij, (len(set_K),len(set_K)))
+
+upd_Var_Bij = []
+for i in dic_Var_Bij.values():
+    upd_Var_Bij.append(i)
+ar_Var_Bij = np.reshape(upd_Var_Bij, (len(set_K),len(set_K)))
+
+upd_Var_dx = []
+for i in dic_Var_dx.values():
+    upd_Var_dx.append(i)
+ar_Var_dx = np.reshape(upd_Var_dx, (len(set_K),len(set_K)))
+
+upd_Var_dy = []
+for i in dic_Var_dy.values():
+    upd_Var_dy.append(i)
+ar_Var_dy = np.reshape(upd_Var_dy, (len(set_K),len(set_K)))
+
 
 #------------------------Affichage------------------------#
 
-print(f"\nX:\n {Var_X}")
-print(f"\nY:\n {Var_Y}")
-print(f"\nAij:\n {Var_Aij}")
-print(f"\nBij:\n {Var_Bij}")
-print(f"\ndx:\n {Var_dx}")
-print(f"\ndy:\n {Var_dy}")
+""" DataFrame """
+print(f"\nX:\n {df_Var_X}")
+print(f"\nY:\n {df_Var_Y}")
+print(f"\nAij:\n {df_Var_Aij}")
+print(f"\nBij:\n {df_Var_Bij}")
+print(f"\ndx:\n {df_Var_dx}")
+print(f"\ndy:\n {df_Var_dy}")
+
+""" Dictionnaire """
+# print(f"\nX:\n {dic_Var_X}")
+# print(f"\nY:\n {dic_Var_Y}")
+# print(f"\nAij:\n {dic_Var_Aij}")
+# print(f"\nBij:\n {dic_Var_Bij}")
+# print(f"\ndx:\n {dic_Var_dx}")
+# print(f"\ndy:\n {dic_Var_dy}")
+
+""" Array """
+# print(f"\n{ar_Var_X} X")
+# print(f"\n{ar_Var_Y} Y")
+# print(f"\n{ar_Var_Aij} Aij")
+# print(f"\n{ar_Var_Bij} Bij")
+# print(f"\n{ar_Var_dx} dx")
+# print(f"\n{ar_Var_dy} dy")
 
