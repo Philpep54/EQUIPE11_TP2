4d8065897108ce77e909bb3fb7158c017d7e384e
Phillipe Pépin <phillipe.pepin.1@ulaval.ca>
2021-04-30 16:58:22 -0400

    fini pour vrai

--- src/TP2_pytosql.py
+++ src/TP2_pytosql.py
@@ -10,65 +10,62 @@
 
 import sqlite3
 from TP2_dat import Solver
+from TP2_Affichage import dic_Var_X, dic_Var_Y, dic_Var_Aij, dic_Var_Bij, dic_Var_dx, dic_Var_dy, dic_Var_ddx, dic_Var_ddy
 import numpy as np
 from data import *
 
 
-""" Résoudre le problème """
-inst = Solver(n, m, d, L, lon_u, lar_u, set_K, lon, poids, cd, Xa, Ya)
-res = inst.solve()
-
 """ Création des strings à partir des résultats de variables """
-x = res[0]
+x = dic_Var_X
 upd_Var_X = []
 for i in x.values():
     upd_Var_X.append(i)
 ar_Var_X = np.reshape(upd_Var_X, len(set_K))
 X = str(ar_Var_X)
 
-y = res[1]
+y = dic_Var_Y
 upd_Var_Y = []
 for i in y.values():
     upd_Var_Y.append(i)
 ar_Var_Y = np.reshape(upd_Var_Y, len(set_K))
 Y = str(ar_Var_Y)
 
-aij = res[2]
+aij = dic_Var_Aij
 upd_Var_Aij = []
 for i in aij.values():
     upd_Var_Aij.append(i)
 ar_Var_Aij = np.reshape(upd_Var_Aij, (len(set_K),len(set_K)))
 Aij = str(ar_Var_Aij)
 
-bij = res[3]
+bij = dic_Var_Bij
 upd_Var_Bij = []
 for i in bij.values():
     upd_Var_Bij.append(i)
 ar_Var_Bij = np.reshape(upd_Var_Bij, (len(set_K),len(set_K)))
 Bij = str(ar_Var_Bij)
 
-dx = res[4]
+dx = dic_Var_dx
 upd_Var_dx = []
 for i in dx.values():
     upd_Var_dx.append(i)
 ar_Var_dx = np.reshape(upd_Var_dx, (len(set_K),len(set_K)))
 Dx = str(ar_Var_dx)
 
-dy = res[5]
+dy = dic_Var_dy
 upd_Var_dy = []
 for i in dy.values():
     upd_Var_dy.append(i)
 ar_Var_dy = np.reshape(upd_Var_dy, (len(set_K),len(set_K)))
 Dy = str(ar_Var_dy)
 
-ddx = res[6]
+ddx = dic_Var_ddx
 upd_Var_ddx = []
 for i in ddx.values():
     upd_Var_ddx.append(i)
 ar_Var_ddx = np.reshape(upd_Var_ddx, len(set_K))
 DDx = str(ar_Var_ddx)
 
-ddy = res[6]
+ddy = dic_Var_ddy
 upd_Var_ddy = []
 for i in ddy.values():
     upd_Var_ddy.append(i)
